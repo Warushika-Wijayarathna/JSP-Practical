a0c756af3e09a81f7a7b1593d6785c61d536d624
Warushika-Wijayarathna <warushikawijayarathna@gmail.com>
2025-01-11 09:22:18 +0530

    1st commit

---
+++ src/main/java/org/example/jsppractical/dto/CustomerDTO.java
@@ -0,0 +1,16 @@
+package org.example.jsppractical.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CustomerDTO {
+
+    private String id;
+    private String name;
+    private String email;
+
+}
---
+++ src/main/java/org/example/jsppractical/entity/Customer.java
@@ -0,0 +1,28 @@
+package org.example.jsppractical.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "Customer")
+public class Customer {
+
+    @Id
+    @Column(name = "cust_id", nullable = false, unique = true)
+    private String id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+}
